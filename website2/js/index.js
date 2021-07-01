4486da6a64cf410eea102da16b3ba26a2c61536e
RAza ALy <007razajutt@gmail.com>
2021-07-01 12:47:36 +0500

    initial commit

---
+++ website2/js/index.js
@@ -0,0 +1,18 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar-example2'
+})
+
+
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl )
+})
+
+toastList[0].show()
